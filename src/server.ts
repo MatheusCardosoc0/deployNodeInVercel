6db386bdc977d1be6bf47078ff69f09445e8655f
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-03-03 11:18:26 -0300

    criando estrutura para teste

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import { Book, PrismaClient } from '@prisma/client'
+import express, { Request, Response } from 'express'
+
+const app = express()
+app.use(express.json())
+
+const prisma = new PrismaClient()
+const port = process.env.PORT ?? 3030
+
+app.get('/', async (req, res) => {
+  
+  const response = await prisma.book.findMany()
+
+  return res.json(response)
+})
+
+app.post('/register', async ( req: Request, res: Response) => {
+
+  const {name, price}: Book = req.body
+
+  const createBook = await prisma.book.create({
+    data:{
+      name,
+      price
+    }
+  })
+
+  return res.json(createBook)
+})
+
+app.listen(port)
